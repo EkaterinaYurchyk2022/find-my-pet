c23990f142280f21eb379688d8711da8946732f7
EkaterinaYurchyk2022 <ekaterina.yurchyk.s@gmail.com>
2022-12-10 23:23:35 +0300

    Added mapbox form to add new pet, store data in localStorage, display all pets on map with popups.

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import './App.css';
+import React from 'react';
+import mapboxgl from 'mapbox-gl';
+import { Main } from './views/main';
+import {
+  BrowserRouter,
+  Route,
+  Link,
+  Routes,
+} from "react-router-dom";
+import { AddPetForm } from './maps/mapbox/add-pet-form';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWthYmFub3YiLCJhIjoiY2w3MGJlbHd5MGNxZzN3cmwxN2F6bGZxMCJ9.z6oW3CWUf_WNwPeENzMsFw';
+
+const App: React.FC = () => {
+    return (
+        <div className="app p-5">
+            Find my pet
+
+            <BrowserRouter>
+                <nav>
+                    <Link to="/">Home</Link> |{" "}
+                    <Link to="/add-mapbox">Add via Mapbox</Link>
+                </nav>
+                <Routes>
+                    <Route path="/add-leaflet"/>
+                    <Route path="/add-mapbox" element={<AddPetForm />} />
+                    <Route path="/" element={ <Main />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
+}
