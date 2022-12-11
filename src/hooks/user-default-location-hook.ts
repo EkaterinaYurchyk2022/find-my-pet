5dc1053fc124cd5c83ac2f5d1fc603abe1926f25
EkaterinaYurchyk2022 <ekaterina.yurchyk.s@gmail.com>
2022-12-11 10:54:50 +0300

    Added leaflet icons, store zoom in user location hook

--- src/hooks/user-default-location-hook.ts
+++ src/hooks/user-default-location-hook.ts
@@ -2,14 +2,16 @@ import {useEffect, useState} from 'react';
 import {Coordinates} from '../models';
 
 function useUserDefaultLocation(postion?: GeolocationCoordinates) {
-    const [userLocation, setUserLocation] = useState<Coordinates>({
+    const [userLocation, setUserLocation] = useState<Coordinates & { zoom: number }>({
         lat: postion?.latitude ?? -70.9,
-        lng: postion?.longitude ?? 42.35
+        lng: postion?.longitude ?? 42.35,
+        zoom: 13
     });
+
 
     useEffect(() => {
         if (postion) {
-            setUserLocation({lat: postion.latitude, lng: postion.longitude})
+            setUserLocation({lat: postion.latitude, lng: postion.longitude, zoom: 13})
         }
     }, [postion]);
 
